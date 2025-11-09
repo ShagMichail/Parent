1cf126b329e7f3528ce3e49761d28839dbd9113f
ShagMichail <shiga2001@mail.ru>
2025-11-10 01:14:23 +0300

    Initial Commit

---
+++ Parent/ParentApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParentApp.swift
+//  Parent
+//
+//  Created by Михаил Шаговитов on 10.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct ParentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
